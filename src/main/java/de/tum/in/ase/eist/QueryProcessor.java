c58215fbb28f54036eccb2e5bb312a62b08c50b7
Vrishab Dev <vrishabdev.vm@gmail.com>
2022-07-13 15:28:51 +0200

    answered with mf

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,7 +13,10 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "MyTeam";
-        } else { // TODO extend the programm here
+        } else if (query.contains("what is your name")) {
+            return "mf";
+        }
+        else { // TODO extend the programm here
             return "";
         }
     }
