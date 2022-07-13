baefff8b7dde80a3688317950612f1558350f33e
Vrishab Dev <vrishabdev.vm@gmail.com>
2022-07-13 15:45:30 +0200

    largest number

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -11,20 +11,10 @@ public class QueryProcessor {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
-        } else if (query.contains("name")) {
-           return "MyTeam";
-        } else if (query.contains("which of the following numbers is the largest: 84, 469")) {
-            return "469";
-        } else if(query.contains("what is 10 plus 15")) {
-            return "25";
-        } else if(query.contains("what is 2 plus 0")) {
-            return "2";
-        } else if(query.contains("what is 17 plus 9")) {
-            return "26";
-        } else if(query.contains("what is 6 plus 17")) {
-            return "23";
         } else if(query.contains("which%20of%20the%20following%20numbers%20is%20both%20a%20square%20and%20a%20cube:%20652,%201156,%20256,%2064")) {
             return "64";
+        } else if(query.contains("%20which%20of%20the%20following%20numbers%20is%20the%20largest:%2071,%20521,%20124,%2044")) {
+            return "521";
         }
         else { // TODO extend the programm here
             return "";
